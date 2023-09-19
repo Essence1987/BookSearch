d3c5da6acbfdfb87c7ed5dbfc88a05b57c651f0e
Essence1987 <129245370+Essence1987@users.noreply.github.com>
2023-09-18 18:09:25 -0600

    set up first schema for Book. still need to do Query and Mutations

--- Develop/server/models/Book.js
+++ Develop/server/models/Book.js
@@ -1,31 +1,66 @@
-const { Schema } = require('mongoose');
+// const { Schema } = require('mongoose');
 
-// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
-const bookSchema = new Schema({
-  authors: [
-    {
-      type: String,
-    },
-  ],
-  description: {
-    type: String,
-    required: true,
-  },
-  // saved book id from GoogleBooks
-  bookId: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  link: {
-    type: String,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-});
+// // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
+// const bookSchema = new Schema({
+//   authors: [
+//     {
+//       type: String,
+//     },
+//   ],
+//   description: {
+//     type: String,
+//     required: true,
+//   },
+//   // saved book id from GoogleBooks
+//   bookId: {
+//     type: String,
+//     required: true,
+//   },
+//   image: {
+//     type: String,
+//   },
+//   link: {
+//     type: String,
+//   },
+//   title: {
+//     type: String,
+//     required: true,
+//   },
+// });
 
-module.exports = bookSchema;
+// module.exports = bookSchema;
+
+const {gql} = require('apollo-server-express');
+const express = require('express');
+
+const booktypeDefs = gql`
+type Book {
+    _id: ID!
+    authors: [String]!
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
+
+input BookInput {
+    authors: [String]!
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
+
+type Query {
+
+}
+
+type Mutation {
+
+}
+
+`;
+
+module.exports = booktypeDefs;
